17674ba858683afb7d1537092e663782c3a13faf
J Cheyo Jimenez <cheyo@masters3d.com>
2017-07-14 16:03:44 -0700

    added JSON file

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,12 @@
 /// <reference types="jquery" />
 'use strict';
 
-let projects = (new Data()).projects;
-
-//Add all the articles to the page
-projects.forEach(function(project){
-  let html = project.toHtml()
-  $('#articles').append(html);
+// Load JSON and popluate the projects data
+$.getJSON( 'data.json', function( response ) {
+  let projects = (new Data(response)).projects
+  //Add all the articles to the page
+  projects.forEach(function(project){
+    let html = project.toHtml()
+    $('#articles').append(html);
+  });
 });
-
-
--- scripts/model.js
+++ scripts/model.js
@@ -28,72 +28,19 @@ Project.prototype.getId = function() {
 }
 
 class Data { // eslint-disable-line
-  constructor() {
-    let projectsInfo = [
-      `art,3D Sketches,http://community.foundry.com/profile/cheyo84,
-        http://content.luxology.com/gallery/7faba61b214ef99f765daea6a422308e.jpg`,
-      `art,All Vimeo Videos,https://vimeo.com/masters3d`,
-      `vid,Honduras 2015 Highlights - Upon this Rock Ministries,youtube=waXta2PAjfc`,
-      `vid,Guatemala 2014 Upon This Rock Ministries,youtube=VMkDSfq1ghg`,
-      `vid,Team returns one year after Yolanda,youtube=hMHgUtxMiG8`,
-      `vid,Thank you vid,youtube=64kGSkgOUjU`,
-      `vid,Change Giving to Going | Hotes Foundation,youtube=8qETFdq6UN8`,
-      `vid,Earthquake,youtube=16NcsqhRhzE`,
-      `vid,A Haitian Prince Turns One,youtube=fd5OF4I45ak`,
-      `vid,Cool 3d After FX,vimeo=1962476`,
-      `vid,TVida Vision,vimeo=1960049`,
-      `vid,Slavador trip,vimeo=1924143`,
-      `vid,WP Storage Structure,youtube=wRj1z21ITDI`,
-      `vid,WP School Canopies,youtube=vOb_Xu74ras`,
-      `vid,AKS Disaster Response Camp,youtube=1q_1wL522yA`,
-      `vid,AKS Quonset Buildings,youtube=3Qz_OsdHruY`,
-      `app,All Github Projects,https://github.com/masters3d?tab=repositories`,
-      `app,BlogClient,https://github.com/masters3d/BlogClient/,
-        https://user-images.githubusercontent.com/6539412/28151810-dea0b682-6751-11e7-8700-a17b41a4492b.gif`,
-      `app,VirtualTourist,https://github.com/masters3d/VirtualTourist,
-        https://cloud.githubusercontent.com/assets/6539412/19018530/da18a9f2-8819-11e6-8766-42ccdc0ea546.gif`,
-      `app,OntheMap,https://github.com/masters3d/OnTheMap,
-        https://cloud.githubusercontent.com/assets/6539412/19424719/5ce11b22-93e0-11e6-85bd-3e2ca49a45b9.png`,
-      `app,BreakoutGame,https://github.com/masters3d/breakoutGame,
-        https://camo.githubusercontent.com/555990b2d77f31959922c4ea545d6d2201aa238a/687474703a2f2f692e696d6775722e636f6d2f395233494a33312e676966`,
-      `app,MemeCreator,https://github.com/masters3d/memeCreatorApp,
-        https://cloud.githubusercontent.com/assets/6539412/19424696/262c5a4c-93e0-11e6-95a2-7a380e6644c7.png`,
-      `app,VoiceRecorder,https://github.com/masters3d/recordVoiceApp,
-        https://user-images.githubusercontent.com/6539412/28151962-c8f77dba-6752-11e7-8ba8-a8f031caa3fb.gif`,
-      `app,JavaGUIProject,https://github.com/masters3d/JavaGUIProject,
-        https://user-images.githubusercontent.com/6539412/28152784-11b0ec7c-6757-11e7-8dd2-b634b3e3bb74.gif`,
-      `app,TextBasedJavaGame,https://github.com/masters3d/TextBasedJavaGame,
-        https://user-images.githubusercontent.com/6539412/28152533-e62a4e32-6755-11e7-81b6-ef309edf99e5.gif`,
-      `app,JavaGUITravelApp,https://github.com/masters3d/JavaGUITravelApp,
-        https://cloud.githubusercontent.com/assets/6539412/14809215/613f446a-0b40-11e6-9cb1-4d1deb177f26.png`,
-      `web,App Engine Wiki Web Server,https://github.com/masters3d/Appengine-Udacity-Blog-Project,
-        http://cheyomasters3d.apppot.com/wiki`,
-      `web,Blog Backend API with User Authentication,http://cheyomasters3d.apppot.com/blog.json,
-        http://cheyomasters3d.appspot.com/blog`,
-      `web,Emoji Slots Game,https://github.com/AlexithymiaStudios/alexi-slots,
-        https://alexithymiastudios.github.io/alexi-slots/`,
-      `web,BusMall Shopping App,https://github.com/masters3d/bus-mall,
-        https://masters3d.github.io/bus-mall/`,
-      `web,Pats Company Site,https://github.com/masters3d/pats_co,
-        https://masters3d.github.io/pats_co/`,
-      `web,About Me Game,https://github.com/masters3d/about_me,
-        https://masters3d.github.io/about_me/requirement/index.html`,
-      `web,Image to HTML Sample,https://github.com/masters3d/chocolate_pizza/,
-        https://masters3d.github.io/chocolate_pizza/`,
-    ]
+  /** @param {Object[]} jsonData */ 
+  constructor(jsonData) {
+
     /** @type {Project[]} _projects */
     let _projects = []
-    for(let each of projectsInfo) {
-      let data = each.split(',')
-      let type = data[0];
-      let name = data[1]
-      let link = data[2]
-      let media = data[3] || ''
-      let project = new Project(type, name, link, media, '', '')
+
+    for(let each of jsonData) {
+      let project = new Project(each.type, each.name, each.link, each.media, each.description, each.date)
       _projects.push(project)
     }
     this._projects = _projects;
   }
+
   get projects() {
     return this._projects
   }
