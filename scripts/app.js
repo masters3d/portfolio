2e552bc44e46974cdc0e1c73859f6ea6bca7a610
J Cheyo Jimenez <cheyo@masters3d.com>
2017-07-17 17:59:08 -0700

    added more SMACSS

--- scripts/app.js
+++ scripts/app.js
@@ -4,24 +4,26 @@
 // Point of Entry of the app
 // Load JSON and popluate the projects data
 $(function() {
-  $.getJSON( 'data/projects.json', function( response ) {
-    let projects = (new Data(response)).projects
-    //Add all the articles to the page
-    for (const project of projects){
-      let html = project.toHtml()
-      $('#articles').append(html);
-    }
-    // Toggles time shows as days since today
-    Controller.timeHoverRegister();
-    // Adds the game menu to header
-    $('header').first().append(Controller.createMenuHtml())
-
-    // Handlers
-    Controller.handlerForNav()
-    Controller.handlerShowAndHide()
-
-    // hiding all the media sections
-    $('.media').hide()
-  });
+  $.getJSON( 'data/projects.json', setup);
 })
 
+/** @param {Object[]} jsonProjectObjects */ 
+function setup(jsonProjectObjects) {
+  let projects = (new Data(jsonProjectObjects)).projects
+  //Add all the articles to the page
+  for (const project of projects){
+    let html = project.toHtml()
+    $('#articles').append(html);
+  }
+  // Toggles time shows as days since today
+  Controller.timeHoverRegister();
+  // Adds the game menu to header
+  $('header').first().append(Controller.createMenuHtml())
+
+  // Handlers
+  Controller.handlerForNav()
+  Controller.handlerShowAndHide()
+
+  // hiding all the media sections
+  $('.media').hide()
+}
