69222aedd8a635e56c7bee95f8abf946239c9029
J Cheyo Jimenez <cheyo@masters3d.com>
2017-07-19 11:03:38 -0700

    added server for the portfolio

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 'use strict';
+const port = parseInt(process.env.PORT || '3000')
+const express = require('express')
+var app = express()
+
+app.use(express.static('.'))
+
+app.listen(port, function() {
+  console.log(`App is running on port ${port}`)
+})
